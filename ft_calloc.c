077a4e4a7ade998a76681f9d418bfb22f9a03b50
maaike-slt <maaikesloot@home.nl>
2023-10-27 16:20:40 +0200

    calloc

---
+++ ft_calloc.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 16:03:45 by msloot            #+#    #+#             */
+/*   Updated: 2023/10/27 16:07:20 by msloot           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_calloc(size_t nmemb, size_t size)
+{
+	void	*dst;
+
+	dst = malloc(nmemb * size);
+	if (!dst)
+		return (NULL);
+	ft_bzero(dst, nmemb * size);
+	return (dst);
+}
