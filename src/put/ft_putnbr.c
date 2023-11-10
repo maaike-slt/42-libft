4d2e66ff4dce508bdc0491657d3889a970ccfeb6
msloot <msloot@student.42.fr>
2023-11-10 22:38:03 +0100

    `putnbr`

--- src/put/ft_putnbr.c
+++ src/put/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 22:16:37 by msloot            #+#    #+#             */
-/*   Updated: 2023/11/10 22:18:09 by msloot           ###   ########.fr       */
+/*   Updated: 2023/11/10 22:35:44 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 inline void	ft_putnbr(int n)
 {
-    return (ft_putnbr_fd(n, STDIN_FILENO));
+	return (ft_putnbr_fd(n, STDIN_FILENO));
 }
