591569731b534e46b80a1817f9d9e41279200d1c
msloot <msloot@student.42.fr>
2023-11-18 17:41:22 +0100

    no STDIN

--- src/put/ft_putchar.c
+++ src/put/ft_putchar.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 16:42:56 by msloot            #+#    #+#             */
-/*   Updated: 2023/11/15 12:04:15 by msloot           ###   ########.fr       */
+/*   Updated: 2023/11/18 17:38:35 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 inline ssize_t	ft_putchar(char c)
 {
-	return (ft_putchar_fd(c, STDIN_FILENO));
+	return (ft_putchar_fd(c, STDOUT_FILENO));
 }
--- src/put/ft_putnbr.c
+++ src/put/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 22:16:37 by msloot            #+#    #+#             */
-/*   Updated: 2023/11/15 13:18:32 by msloot           ###   ########.fr       */
+/*   Updated: 2023/11/18 17:38:32 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,5 @@
 
 inline ssize_t	ft_putnbr(int n)
 {
-	return (ft_putnbr_fd(n, STDIN_FILENO));
+	return (ft_putnbr_fd(n, STDOUT_FILENO));
 }
