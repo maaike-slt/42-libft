a88767073c0e7c4529dac48ab4c4c3786c956f3f
msloot <msloot@student.42.fr>
2023-11-18 17:21:52 +0100

    fixed segfault

--- src/put/ft_putnbr_base.c
+++ src/put/ft_putnbr_base.c
@@ -6,7 +6,7 @@
 /*   By: msloot <msloot@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/27 10:55:10 by msloot            #+#    #+#             */
-/*   Updated: 2023/11/18 17:13:09 by msloot           ###   ########.fr       */
+/*   Updated: 2023/11/18 17:19:40 by msloot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,12 @@
 
 ssize_t	ft_putnbr_base(int n, const char *base)
 {
-	char	*str;
+	char	str[12];
 	size_t	base_len;
 
 	base_len = ft_check_base(base);
 	if (base_len == 0)
 		return (-16);
-	str = NULL;
 	ft_intcpy_base(str, n, base, base_len);
 	return (ft_putstr(str));
 }
